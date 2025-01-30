2eb98c1968b8ddbfd84c6c7c2f181ec85f426641
Mbali-Phakathie <phakathie1@gmail.com>
2025-01-31 01:25:01 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,35 +1,15 @@
-"""littlelemon URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from restaurant.views import BookingViewSet
-
-router = DefaultRouter()
-router.register(r'tables', BookingViewSet)
-
-def redirect_view(request):
-    return redirect('/restaurant/')
+from django.urls import path
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', redirect_view),
-    path("admin/", admin.site.urls),
-    path("restaurant/", include('restaurant.urls')),
-    path("restaurant/booking/", include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('', views.index, name='index'),
+    path('book/', views.book, name="book"),
+    path('about/', views.about, name="about"),
+    path('menu/', views.menu, name="menu"),
+    path('bookings', views.bookings, name="bookings"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
